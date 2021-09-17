ad9197a1d49bfcc435e7bcf163be7c0d6493015f
Oliver Schneider <o.schneider@EDU.epsb.ca>
2021-09-17 14:49:54 -0600

    finished level 1-10

--- solutions.js
+++ solutions.js
@@ -6,68 +6,112 @@ let row, column;
 // Solution Functions...
 function level1Solution() {
 	// USE BINARY IF STATEMENT
-	
-	document.getElementById("currentTile").innerHTML = "----"; // Set output text
+	if (column <= 4) {
+		document.getElementById("currentTile").innerHTML = "Sand";
+	} else {
+		document.getElementById("currentTile").innerHTML = "Water";
+	}
 }
 
 function level2Solution() {
 	// USE CHAINED IF STATEMENT
-	
-	document.getElementById("currentTile").innerHTML = "----"; // Set output text
+	if (column <= 2) {
+		document.getElementById("currentTile").innerHTML = "Hills";
+	} else if (column <= 7) {
+		document.getElementById("currentTile").innerHTML = "Sand";
+	} else {
+		document.getElementById("currentTile").innerHTML = "Water";
+	}
 }
 
 function level3Solution() {
 	// USE BINARY IF STATEMENT
-	
-	document.getElementById("currentTile").innerHTML = "----"; // Set output text
+	if (column >= 3 && column <= 7) {
+		document.getElementById("currentTile").innerHTML = "Sand";
+	} else {
+		document.getElementById("currentTile").innerHTML = "Hills";
+	}
 }
 
 function level4Solution() {
 	// USE BINARY IF STATEMENT
-
-	document.getElementById("currentTile").innerHTML = "----"; // Set output text
+	if (row <= 2) {
+		document.getElementById("currentTile").innerHTML = "Hills";
+	} else
+	document.getElementById("currentTile").innerHTML = "Sand";
 }
 
 function level5Solution() {
 	// USE CHAINED IF STATEMENT
-
-	document.getElementById("currentTile").innerHTML = "----"; // Set output text
+	if (row <= 2) {
+		document.getElementById("currentTile").innerHTML = "Hills";
+	} else if(row <= 5) {
+		document.getElementById("currentTile").innerHTML = "Sand";
+	} else {
+		document.getElementById("currentTile").innerHTML = "Water";
+	}
 }
 
 function level6Solution() {
 	// USE BINARY IF STATEMENT
-
-	document.getElementById("currentTile").innerHTML = "----"; // Set output text
+	if (row >= 2 && row <= 5) {
+		document.getElementById("currentTile").innerHTML = "Sand";
+	} else {
+		document.getElementById("currentTile").innerHTML = "Water";
+	}
 }
 
 function level7Solution() {
 	// USE BINARY IF STATEMENT
-
-	document.getElementById("currentTile").innerHTML = "----"; // Set output text
+	if (row === 4 && column === 7) {
+		document.getElementById("currentTile").innerHTML = "Water";
+	} else {
+		document.getElementById("currentTile").innerHTML = "Sand";
+	}
 }
 
 function level8Solution() {
 	// USE BINARY IF STATEMENT
-
-	document.getElementById("currentTile").innerHTML = "----"; // Set output text
+	if (column === 2 || row === 5) {
+		document.getElementById("currentTile").innerHTML = "Tree";
+	} else {
+		document.getElementById("currentTile").innerHTML = "Sand";
+	}
 }
 
 function level9Solution() {
 	// USE BINARY IF STATEMENT
-
-	document.getElementById("currentTile").innerHTML = "----"; // Set output text
+	if (column <= 5 && row <=5) {
+		document.getElementById("currentTile").innerHTML = "Water";
+	} else {
+		document.getElementById("currentTile").innerHTML = "Sand";
+	}
 }
 
 function level10Solution() {
 	// USE CHAINED IF STATEMENT
-
-	document.getElementById("currentTile").innerHTML = "----"; // Set output text
+	if (column <= 1) {
+		document.getElementById("currentTile").innerHTML = "Hills";
+	} else if (column <= 3) {
+		document.getElementById("currentTile").innerHTML = "Tree";
+	} else if (column <= 7) {
+		document.getElementById("currentTile").innerHTML = "Sand";
+	} else {
+		document.getElementById("currentTile").innerHTML = "Water";
+	}
 }
 
 function level11Solution() {
 	// USE CHAINED IF STATEMENT
-
-	document.getElementById("currentTile").innerHTML = "----"; // Set output text
+	if (column <= 5 && row <= 3) {
+		document.getElementById("currentTile").innerHTML = "Water";
+	} else if (column >= 6 && row <=3) {
+		document.getElementById("currentTile").innerHTML = "Tree";
+	} else if () {
+		document.getElementById("currentTile").innerHTML = "----";
+	} else {
+		document.getElementById("currentTile").innerHTML = "----";
+	}
 }
 
 function level12Solution() {
